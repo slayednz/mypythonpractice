b3312acde7b3c857b7639a17553b6f65a70f1c87
SlayedNZ <132971830+slayednz@users.noreply.github.com>
2024-09-24 11:04:22 +1200

    basic sets from Harvard lectures

---
+++ simplepractice/gryffindor/gryffindor.py
@@ -0,0 +1,18 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
+
+gryffindor = []
+
+for student in students:
+    gryffindor.append({"name": student, "house": "Gryffindor"})
+
+
+gryffindors = filter(lambda s: s['house'] == "Gryffindor", students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
